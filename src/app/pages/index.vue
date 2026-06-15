4b9b5b0bdff1c447d132167d38a6297129043150
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2026-06-15 09:37:16 +0000

    refactor: remove UPageHero from unauth index

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
-import { description, name as title } from '@@/package.json'
-
 definePageMeta({
   layout: 'unauth',
 })
 </script>
 
 <template>
-  <div>
-    <UPageHero
-      :title="title"
-      :description="description"
-    />
-  </div>
+  <div />
 </template>
